29eec08052035bd923b7ef896fb52928307d07fb
mpg-teruhisa-fukumoto <teruhisa.fukumoto@medpeer.co.jp>
2021-03-17 20:25:16 +0900

    [Add]Setting i18n for :ja

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,14 @@ module Myapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # TimeZone
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    # Setting locales
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
